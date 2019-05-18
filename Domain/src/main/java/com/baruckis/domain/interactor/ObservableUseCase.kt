f1f7921ab5706546e35278a5952bc0bad07a94c7
Andrius Baruckis <andrius.baruckis@gmail.com>
2019-05-18 20:43:33 +0300

    feat: Setting up the Domain Layer Interactor class.

---
+++ Domain/src/main/java/com/baruckis/domain/interactor/ObservableUseCase.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019 Andrius Baruckis www.baruckis.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.baruckis.domain.interactor
+
+import com.baruckis.domain.executor.PostExecutionThread
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableObserver
+import io.reactivex.schedulers.Schedulers
+
+abstract class ObservableUseCase<T, in Params> constructor(private val postExecutionThread: PostExecutionThread) {
+
+    private val disposables = CompositeDisposable()
+
+    abstract fun buildUseCaseObservable(params: Params? = null): Observable<T>
+
+    open fun execute(observer: DisposableObserver<T>, params: Params? = null) {
+        val observable = this.buildUseCaseObservable(params)
+            .subscribeOn(Schedulers.io())
+            .observeOn(postExecutionThread.scheduler)
+        addDisposable(observable.subscribeWith(observer))
+    }
+
+    fun dispose() {
+        disposables.dispose()
+    }
+
+    private fun addDisposable(disposable: Disposable) {
+        disposables.add(disposable)
+    }
+
+}
