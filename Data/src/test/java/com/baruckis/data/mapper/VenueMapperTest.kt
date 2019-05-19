fbe3a681e22cb33eade8da8f8e17290f62e796e7
Andrius Baruckis <andrius.baruckis@gmail.com>
2019-05-19 20:51:14 +0300

    test: Testing the Venue entity mapper.

---
+++ Data/src/test/java/com/baruckis/data/mapper/VenueMapperTest.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2019 Andrius Baruckis www.baruckis.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.baruckis.data.mapper
+
+import com.baruckis.data.model.VenueEntity
+import com.baruckis.domain.model.Venue
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class VenueMapperTest {
+
+    private val mapper = VenueMapper()
+
+    @Test
+    fun mapFromEntity() {
+
+        val entity = VenueEntity("Trafalgar Sq", 51.50812811764834, -0.12808620929718018)
+        val model = mapper.mapFromEntity(entity)
+
+        assertMapsDataCorrectly(entity, model)
+    }
+
+    @Test
+    fun mapToEntity() {
+
+        val model = Venue("Gedimino pr.", 54.68736449150992, 25.279981398558263)
+        val entity = mapper.mapToEntity(model)
+
+        assertMapsDataCorrectly(entity, model)
+    }
+
+    private fun assertMapsDataCorrectly(entity: VenueEntity, model: Venue) {
+        assertEquals(entity.name, model.name)
+        assertEquals(entity.lattitude, model.lattitude)
+        assertEquals(entity.longitude, model.longitude)
+    }
+
+}
