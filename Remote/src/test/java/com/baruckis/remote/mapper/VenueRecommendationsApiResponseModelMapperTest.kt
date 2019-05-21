fe45ef30fd36a2448232da9016384d4b39830fd2
Andrius Baruckis <andrius.baruckis@gmail.com>
2019-05-21 17:10:58 +0300

    test: Setting up the Remote layer for testing. Testing the remote model mapper class.

---
+++ Remote/src/test/java/com/baruckis/remote/mapper/VenueRecommendationsApiResponseModelMapperTest.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019 Andrius Baruckis www.baruckis.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.baruckis.remote.mapper
+
+import com.baruckis.remote.model.Item
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class VenueRecommendationsApiResponseModelMapperTest {
+
+    private val mapper = VenueRecommendationsApiResponseModelMapper()
+
+    @Test
+    fun mapFromApiResponseModel() {
+
+        val model = Item(
+            Item.Venue("Vingio parkas", Item.Venue.Location(54.68293703261666, 25.237655639648438))
+        )
+        val entity = mapper.mapFromApiResponseModel(model)
+
+        assertEquals(model.venue.name, entity.name)
+        assertEquals(model.venue.location.lat, entity.lattitude)
+        assertEquals(model.venue.location.lng, entity.longitude)
+    }
+
+}
