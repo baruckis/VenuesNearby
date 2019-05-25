5dfc6c2b1853f72eea58fe022635d14ac933d74a
Andrius Baruckis <andrius.baruckis@gmail.com>
2019-05-25 15:52:51 +0300

    feat: Creating the RecyclerView Adapter with Data Binding.

---
+++ UI/src/main/java/com/baruckis/ui/search/ExploreVenuesRecyclerViewAdapter.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019 Andrius Baruckis www.baruckis.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.baruckis.ui.search
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.baruckis.ui.databinding.ListItemVenueBinding
+import com.baruckis.ui.model.VenueUi
+
+class ExploreVenuesRecyclerViewAdapter(): RecyclerView.Adapter<ExploreVenuesRecyclerViewAdapter.BindingViewHolder>() {
+
+    private var dataList: List<VenueUi> = ArrayList()
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder {
+
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemVenueBinding.inflate(inflater, parent, false)
+
+        return BindingViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: BindingViewHolder, position: Int) = holder.bind(dataList[position])
+
+    override fun getItemCount(): Int = dataList.count()
+
+
+    inner class BindingViewHolder(private var binding: ListItemVenueBinding): RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(venueUi: VenueUi) {
+
+            binding.venueUi = venueUi
+
+            binding.executePendingBindings()
+        }
+
+    }
+
+}
